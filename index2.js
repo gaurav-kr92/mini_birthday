a4ebd1708a0dd2168a19eca3dd15c98223ec68a4
gaurav-kr92 <gaurav.kr9232@gmail.com>
2022-12-26 13:09:15 +0530

    modified:   index2.js

--- index2.js
+++ index2.js
@@ -20,8 +20,8 @@ const notes = [
   {f:523,d:1,t:"Birth",p:p3},
   {f:440,d:1,t:"day&nbsp;",p:p3},
   {f:349,d:1,t:"Dear&nbsp;",p:p3},
-  {f:330,d:1,t:"Dar",p:p3},
-  {f:294,d:3,t:"win",p:p3},
+  {f:330,d:1,t:"Mi",p:p3},
+  {f:294,d:3,t:"ni",p:p3},
   
   {f:466,d:.5,t:"Hap",p:p4},
   {f:466,d:.5,t:"py&nbsp;",p:p4},
